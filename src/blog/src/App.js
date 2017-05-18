1f6ff199118121b0da7193bb9837eb06e9055600
geekodour <hrishikeshbman@gmail.com>
2017-05-19 02:53:30 +0530

    added navbar

--- src/blog/src/App.js
+++ src/blog/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar/>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
---
+++ src/blog/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+      I am navbar
+    </div>
+  );
+}
+
+export default NavBar;
