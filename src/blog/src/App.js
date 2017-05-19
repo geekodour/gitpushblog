a8afc295298f5c6d130da6ee800f7a494eacf74a
geekodour <hrishikeshbman@gmail.com>
2017-05-19 11:55:25 +0530

    added bulma

--- src/blog/src/App.js
+++ src/blog/src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './components/navbar';
+//import NavBar from './components/navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar/>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="columns">
+                  <div className="column">1</div>
+                  <div className="column">2</div>
+                  <div className="column">2</div>
+                  <div className="column">2</div>
+          </div>
       </div>
     );
   }
--- src/blog/src/components/navbar.jsx
+++ src/blog/src/components/navbar.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const NavBar = () => {
   return (
     <div>
-      I am navbar
     </div>
   );
 }
