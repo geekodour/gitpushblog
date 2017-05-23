25dffcb972b9f8348ab9fef890a376bf3c726725
geekodour <hrishikeshbman@gmail.com>
2017-05-23 19:23:46 +0530

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].css"
+});
+
+module.exports = {
+  entry: {
+        Main: './static/main.js',
+  },
+  output: {
+    path: path.join(__dirname,'./build/assets/'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+              test: /\.(js|jsx)$/,
+              use: [
+                {loader:'babel-loader'}
+              ],
+              exclude: /node_modules/
+      },
+      {
+            test: /\.sass$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                },
+                {
+                    loader: "postcss-loader"
+                },
+                {
+                    loader: "sass-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        }
+    ]
+  },
+  plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+                name: 'common',
+                filename: 'bundle.common.js',
+                chunks: []
+
+        }),
+        extractSass
+  ]
+}
